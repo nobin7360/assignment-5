8b728eddb986d5d3466c3129961412fd47f9bbf4
nobin7360 <nobinchandra1@gmail.com>
2025-09-05 02:15:57 +0600

    js code done

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-const heartCount = document.getElementById('heart-count');
+const heartCount=document.getElementById('heart-count');
 const heartBtn=document.querySelectorAll('.fa-heart');
-heartBtn.forEach(btn=>{
+for(const btn of heartBtn){
     btn.addEventListener('click',function(){
         let current=parseInt(heartCount.innerText);
         heartCount.innerText=current+1;
     })
-})
-
-// call button click eventListener
+}
 
 
-const callbtns=document.querySelectorAll('.fa-phone');
+const callbtns=document.querySelectorAll('.call-btn');
 const coins=document.getElementById('coins');
-callbtns.forEach(btn=>{
-    btn.addEventListener('click',()=>{
-        let curretncoin=parseInt(coins.innerText)
-        if (curretncoin<20) {
-              alert("Not enough coins");
-              return;
-        }
-        let remcoin=curretncoin-20;
-        coins.innerText=remcoin;
+const historyData=document.getElementById('history-data');
 
-    })
-    
+function addHistory(serviceName,serviceNum) {
+    let entry=document.createElement('div');
+    entry.style.margin="5px 0";
+    entry.innerHTML=`${serviceName} - ${serviceNum}`;
+    historyData.appendChild(entry);
+}
+
+for(const btn of callbtns){
+    btn.addEventListener('click', () => {
+        let currentcoin=parseInt(coins.innerText);
+
+        if (currentcoin < 20) {
+            alert("Not enough coins.");
+            return;
+        }
+
+        coins.innerText=currentcoin - 20;
+
+        let serviceName=btn.getAttribute("data-name");
+
+
+        let serviceNum=btn.getAttribute("data-number");
+
+        alert(`Calling ${serviceName} (${serviceNum})`);
+        addHistory(serviceName, serviceNum);
+    });
+}
+
+
+document.getElementById('clear-btn').addEventListener('click',()=>{
+    historyData.innerHTML="";
 })
