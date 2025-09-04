80ae9158793acac91356b7b856b0cac94e49aff1
nobin7360 <nobinchandra1@gmail.com>
2025-09-05 04:04:04 +0600

    everything hassbeen added

--- index.js
+++ index.js
@@ -9,34 +9,67 @@ for(const btn of heartBtn){
 
 
 const callbtns=document.querySelectorAll('.call-btn');
+
+const copybtn=document.querySelectorAll('.copy-btn');
+
 const coins=document.getElementById('coins');
 const historyData=document.getElementById('history-data');
 
 function addHistory(serviceName,serviceNum) {
+
     let entry=document.createElement('div');
     entry.style.margin="5px 0";
-    entry.innerHTML=`${serviceName} - ${serviceNum}`;
+
+    let time=new Date().toLocaleTimeString();
+
+
+    entry.innerHTML=`${serviceName}-${serviceNum}(Called at: ${time})`;
+
+    entry.innerHTML=`${serviceName}-${serviceNum}(Called at: ${time})`;
+    
     historyData.appendChild(entry);
 }
 
 for(const btn of callbtns){
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click',()=>{
         let currentcoin=parseInt(coins.innerText);
 
-        if (currentcoin < 20) {
+        if(currentcoin<20){
             alert("Not enough coins.");
             return;
         }
 
-        coins.innerText=currentcoin - 20;
+        coins.innerText=currentcoin-20;
 
         let serviceName=btn.getAttribute("data-name");
 
-
         let serviceNum=btn.getAttribute("data-number");
 
-        alert(`Calling ${serviceName} (${serviceNum})`);
-        addHistory(serviceName, serviceNum);
+        alert(`Calling ${serviceName}(${serviceNum})`);
+        addHistory(serviceName,serviceNum);
+    });
+}
+
+
+
+
+for(const y of copybtn){
+    y.addEventListener('click',()=>{
+        let card=y.parentElement;
+        let callBtn=card.querySelector('.call-btn');
+        let numberText=callBtn.getAttribute('data-number');
+
+        navigator.clipboard.writeText(numberText).then(()=>{
+
+            let copyNumber=document.getElementById('copy-count');
+
+            let currentCopy=parseInt(copyNumber.innerText);
+
+
+
+            copyNumber.innerText=currentCopy+1;
+            alert(`Copied:${numberText}`);
+        });
     });
 }
 
@@ -44,3 +77,4 @@ for(const btn of callbtns){
 document.getElementById('clear-btn').addEventListener('click',()=>{
     historyData.innerHTML="";
 })
+
